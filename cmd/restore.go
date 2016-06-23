da31ac0ac59285dcfcb37d28554ef4da1210566e
Florin Dragos <florin.dragos@hpe.com>
2016-06-23 17:01:10 +0300

    restore user roles

--- cmd/restore.go
+++ cmd/restore.go
@@ -12,6 +12,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	ORG_AUDIT     = "audited_organizations"
+	ORG_BILLING   = "billing_managed_organizations"
+	ORG_MANAGER   = "managed_organizations"
+	ORG_DEV       = "organizations"
+	SPACE_AUDIT   = "audited_spaces"
+	SPACE_MANAGER = "managed_spaces"
+	SPACE_DEV     = "spaces"
+)
+
 type Org struct {
 	Name      string `json:"name"`
 	QuotaGUID string `json:-`
@@ -28,6 +38,37 @@ func showInfo(sMessage string) {
 
 func showWarning(sMessage string) {
 	fmt.Printf("WARNING: %s\n", sMessage)
+}
+
+func restoreUser(user, space, role string) string {
+	showInfo(fmt.Sprintf("Restaurating User: %s", user))
+
+	user_id := getUserId(user)
+	if user_id == "" {
+		showWarning(fmt.Sprintf("Could not find user: %s", user))
+	} else {
+		path := fmt.Sprintf("/v2/users/%s/%s/%s", user_id, role, space)
+		resp, err := CliConnection.CliCommandWithoutTerminalOutput("curl",
+			path, "-X", "PUT")
+		if err != nil {
+			showWarning(fmt.Sprintf("Could not create user association %s, exception message: %s",
+				user, err.Error()))
+		}
+		return showResult(resp, "user", user, false)
+	}
+	return ""
+}
+
+func getUserId(user string) string {
+	cache := make(map[string]interface{})
+	resources := util.GetResources(CliConnection, "/v2/users", 1, nil, cache)
+	for _, u := range *resources {
+		if u.Entity["username"].(string) == user {
+			return u.Metadata["guid"].(string)
+		}
+	}
+
+	return ""
 }
 
 func restoreOrg(org Org) string {
@@ -136,6 +177,49 @@ func showOrgResult(resp []string, org Org) string {
 	}
 }
 
+func showResult(resp []string, entity, name string, checkName bool) string {
+	oResp := make(map[string]interface{})
+	if len(resp) == 0 {
+		showWarning(fmt.Sprintf("Got null response while restoring %s %s",
+			entity, name))
+		return ""
+	}
+	err := json.Unmarshal([]byte(resp[0]), &oResp)
+	if err != nil {
+		showWarning(fmt.Sprintf("Got unknow response while restoring %s %s: %s",
+			entity, name, err.Error()))
+		return ""
+	}
+	if oResp["error_code"] != nil {
+		showWarning(fmt.Sprintf("got %v-%v while restoring %s %s",
+			oResp["error_code"], oResp["description"], entity, name))
+		return ""
+	}
+
+	if checkName {
+		if oResp["entity"] != nil {
+
+			resource := util.TransformToResource(oResp, make(map[string]interface{}), nil)
+			inName := fmt.Sprintf("%v", resource.Entity["name"])
+			if inName == name {
+				showInfo(fmt.Sprintf("Succesfully restored %s %s", entity, name))
+				if resource.Metadata != nil {
+					return resource.Metadata["guid"].(string)
+				}
+			} else {
+				showWarning(fmt.Sprintf("Name %s does not match requested name %s",
+					oResp["name"], name))
+				return ""
+			}
+		} else {
+			showWarning(fmt.Sprintln("\tWarning unknown answer received"))
+			return ""
+		}
+	}
+
+	return ""
+}
+
 func restoreFromJSON() {
 	var fileContent []byte
 	_, err := os.Stat(BackupFile)
@@ -153,11 +237,47 @@ func restoreFromJSON() {
 		org_guid := restoreOrg(o)
 
 		if org_guid != "" {
+			auditors := org.Entity["auditors"].(*[]*models.ResourceModel)
+			for _, auditor := range *auditors {
+				restoreUser(auditor.Entity["username"].(string), org_guid, ORG_DEV)
+				restoreUser(auditor.Entity["username"].(string), org_guid, ORG_AUDIT)
+			}
+
+			billing_managers := org.Entity["billing_managers"].(*[]*models.ResourceModel)
+			for _, manager := range *billing_managers {
+				restoreUser(manager.Entity["username"].(string), org_guid, ORG_DEV)
+				restoreUser(manager.Entity["username"].(string), org_guid, ORG_BILLING)
+			}
+
+			managers := org.Entity["managers"].(*[]*models.ResourceModel)
+			for _, manager := range *managers {
+				restoreUser(manager.Entity["username"].(string), org_guid, ORG_DEV)
+				restoreUser(manager.Entity["username"].(string), org_guid, ORG_MANAGER)
+			}
+
 			spaces := org.Entity["spaces"].(*[]*models.ResourceModel)
 			for _, space := range *spaces {
 				s := Space{Name: space.Entity["name"].(string), OrganizationGuid: org_guid}
-				restoreSpace(s)
+				space_guid := restoreSpace(s)
+
+				if space_guid != "" {
+					auditors := space.Entity["auditors"].(*[]*models.ResourceModel)
+					for _, auditor := range *auditors {
+						restoreUser(auditor.Entity["username"].(string), space_guid, SPACE_AUDIT)
+					}
+
+					developers := space.Entity["developers"].(*[]*models.ResourceModel)
+					for _, developer := range *developers {
+						restoreUser(developer.Entity["username"].(string), space_guid, SPACE_DEV)
+					}
+
+					managers := space.Entity["managers"].(*[]*models.ResourceModel)
+					for _, manager := range *managers {
+						restoreUser(manager.Entity["username"].(string), space_guid, SPACE_MANAGER)
+					}
+				}
 			}
+
 		}
 	}
 }
